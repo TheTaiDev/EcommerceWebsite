58e32e539272c47c07f1b3b3e4281248198d1a01
manhthetaipy <manhthetai2k1@gmail.com>
2023-02-28 03:27:29 +0700

    up

---
+++ src/components/Header/Head.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <section className="head">
+      <div className="container d_flex">
+        <div className="left row">
+            <i className="fa fa-phone"></i>
+            <label htmlFor="">0348408408</label>
+            <i className="fa fa-envelope"></i>
+            <label htmlFor="">manhthetai2k1@gmail.com</label>
+        </div>
+        <div className="right row RText">
+            <label htmlFor="">Theme FAQ's</label>
+            <label htmlFor="">Need Helps</label>
+            <span></span>
+            <label htmlFor="">EN</label>
+            <span></span>
+            <label htmlFor="">USD</label>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Head;
