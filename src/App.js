09a82266069c72e8a8e27789bdd5c589ab16149e
manhthetaipy <manhthetai2k1@gmail.com>
2023-03-01 00:08:05 +0700

    Ghi chú về commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Header from "./components/Header/Header";
 import "./App.css";
+import Pages from "./components/pages/Page";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import Data from "./components/flashDeals/Data";
+import { useState } from "react";
 function App() {
+  // Step 1: f data from database
+  const { productItems } = Data;
+  const [cartItem, setCardItem] = useState([]);
   return (
     <div className="App">
       <Router>
         <Header />
-        <Switch>{/* <Route path="/about"></Route> */}</Switch>
+        <Switch>
+          {/* <Route path="/about"></Route> */}
+          <Route>
+            <Pages productItems={productItems} />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
